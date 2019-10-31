6bf536d413b7b311b2fd670f33243ba8a9e2d7ea
demekeabewa <amare.abewa@gmail.com>
2019-10-31 23:21:20 +0300

    First Commit

---
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ValueComponent } from './value/value.component';
+
+
+const routes: Routes = [
+  {
+    path:'values',
+    component:ValueComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
